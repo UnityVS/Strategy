67f62bc1248ab6c545a5e628706da73acb759368
MemorySharp <letovsy@yandex.ru>
2022-11-27 23:32:13 +0300

    Enemy navmesh states

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -7,9 +7,9 @@ using UnityEngine.UI;
 public enum EnemyStates
 {
     Idle,
-    Attack,
     WalkToBuilding,
-    WalkToUnit
+    WalkToUnit,
+    Attack
 }
 public class Enemy : MonoBehaviour
 {
@@ -21,16 +21,72 @@ public class Enemy : MonoBehaviour
     [SerializeField] float _distanceToAttack;
     [SerializeField] Building _targetBuilding;
     [SerializeField] Unit _targetUnit;
+    [SerializeField] Animator _animator;
     void Start()
     {
-
+        SetEnemyState(EnemyStates.WalkToBuilding);
     }
     void Update()
     {
+        if (_currentEnemyState == EnemyStates.Idle)
+        {
 
+        }
+        else if (_currentEnemyState == EnemyStates.WalkToBuilding)
+        {
+
+        }
+        else if (_currentEnemyState == EnemyStates.WalkToUnit)
+        {
+
+        }
+        else if (_currentEnemyState == EnemyStates.Attack)
+        {
+
+        }
+    }
+    public void SetEnemyState(EnemyStates enemystate)
+    {
+        _currentEnemyState = enemystate;
+        if (_currentEnemyState == EnemyStates.Idle)
+        {
+
+        }
+        else if (_currentEnemyState == EnemyStates.WalkToBuilding)
+        {
+            if (_targetBuilding = FindClosestBuilding())
+            {
+                if (_targetBuilding == null) return;
+                _navMeshAgent.SetDestination(_targetBuilding.transform.position);
+            }
+        }
+        else if (_currentEnemyState == EnemyStates.WalkToUnit)
+        {
+
+        }
+        else if (_currentEnemyState == EnemyStates.Attack)
+        {
+
+        }
     }
     void FindClosestUnit()
     {
 
+    }
+    Building FindClosestBuilding()
+    {
+        Building[] allBuildings = FindObjectsOfType<Building>();
+        float minDistance = Mathf.Infinity;
+        Building closestBuilding = null;
+        for (int i = 0; i < allBuildings.Length; i++)
+        {
+            float distance = Vector3.Distance(transform.position, allBuildings[i].transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                return closestBuilding = allBuildings[i];
+            }
+        }
+        return null;
     }
 }
