1c3f7c94dced846d6059f8aec8f3bb7a5aadabde
MemorySharp <letovsy@yandex.ru>
2022-12-02 02:34:38 +0300

    Many UI fixes

--- Assets/Scripts/UI/ToolTipUGUI.cs
+++ Assets/Scripts/UI/ToolTipUGUI.cs
@@ -1,4 +1,6 @@
 using DG.Tweening;
+using System.Collections;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -10,43 +12,52 @@ public class ToolTipUGUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     [SerializeField] float _fadeInTime = 1.5f;
     [SerializeField] float _opacityText = 0.5f;
     Tween _myTweenUnFade = null;
-    Tween _myTweenFade = null;
-    Tween _myTweenDelay = null;
+    Coroutine _coroutine;
     private void Awake()
     {
         _toolTip.DOFade(0f, 0);
         _toolTip.gameObject.SetActive(false);
     }
-    public void OnPointerEnter(PointerEventData eventData)
+    public async void OnPointerEnter(PointerEventData eventData)
     {
-        _toolTip.gameObject.SetActive(true);
-        ShowText();
+        StartCoroutine(WaitingShowText());
+        //if (_myTweenUnFade != null) return;
+        //_toolTip.gameObject.SetActive(true);
+        //await ShowText();
     }
-    public void OnPointerExit(PointerEventData eventData)
+    public async void OnPointerExit(PointerEventData eventData)
     {
-        HideText(_fadeOutTime);
+        await HideText(_fadeOutTime);
     }
-    void HideText(float fadeOutTime)
+    async Task HideText(float fadeOutTime)
     {
-        _myTweenFade = _toolTip.DOFade(0f, fadeOutTime);
+        if (_myTweenUnFade != null) { await _myTweenUnFade.AsyncWaitForCompletion(); }
+        _myTweenUnFade = _toolTip.DOFade(0f, fadeOutTime);
         Invoke(nameof(NullingFade), fadeOutTime);
     }
     void ShowText()
     {
-        if (_myTweenFade == null && _myTweenUnFade == null)
-        {
-            _myTweenUnFade = _toolTip.DOFade(_opacityText, _fadeInTime);
-        }
+        //if (_myTweenUnFade != null) { await _myTweenUnFade.AsyncWaitForCompletion(); }
+        _myTweenUnFade = _toolTip.DOFade(_opacityText, _fadeInTime);
     }
-    void FadeDelay()
+    IEnumerator WaitingShowText()
     {
-        _myTweenDelay = _toolTip.DOFade(0f, _fadeInTime);
-        Invoke(nameof(NullingFade), _fadeInTime);
+        while (true)
+        {
+            if (_myTweenUnFade == null)
+            {
+                _toolTip.gameObject.SetActive(true);
+                ShowText();
+                StopAllCoroutines();
+            }
+            yield return null;
+        }
     }
     void NullingFade()
     {
-        _myTweenUnFade = null;
-        _myTweenFade = null;
-        _myTweenDelay = null;
+        if (_myTweenUnFade != null)
+        {
+            _myTweenUnFade = null;
+        }
     }
 }
